3477fe05a9919d17ce47404753f79921e411de0e
sik2 <bickwang123@gmail.com>
2024-10-11 11:15:10 +0900

    add entity

--- src/main/java/com/market/apple/domain/article/entity/Article.java
+++ src/main/java/com/market/apple/domain/article/entity/Article.java
@@ -1,6 +1,9 @@
 package com.market.apple.domain.article.entity;
 
+import com.market.apple.domain.member.entity.Member;
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -10,15 +13,20 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long member_id;
+    @ManyToOne
+    private Member member;
+
+    @Column(columnDefinition = "TINYINT(1)")
+    private boolean is_notice;
 
     @Column(length = 100)
     private String title;
 
     @Column(columnDefinition = "TEXT")
     private String content;
+
+    @Column
+    private int count;
 
     private LocalDateTime createDate;
 
--- src/main/java/com/market/apple/domain/member/entity/Member.java
+++ src/main/java/com/market/apple/domain/member/entity/Member.java
@@ -1,12 +1,21 @@
 package com.market.apple.domain.member.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Member {
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    //
 
+    private LocalDateTime createDate;
+
+    private LocalDateTime modifiedDate;
 }
