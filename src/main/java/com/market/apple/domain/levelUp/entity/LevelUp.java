27b334645833edd581cd81456280da4e058653e6
anjddl <anjddl@gmail.com>
2024-10-11 11:45:45 +0900

    add

--- src/main/java/com/market/apple/domain/levelUp/entity/LevelUp.java
+++ src/main/java/com/market/apple/domain/levelUp/entity/LevelUp.java
@@ -1,10 +1,33 @@
 package com.market.apple.domain.levelUp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.market.apple.domain.member.entity.Member;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class LevelUp {
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @Column(length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime modifiedDate;
+
+    public enum Status {
+        W, Y, N
+    }
 }
