b252e62e49dd3bed21abaf822b3c1882ca38efde
sik2 <bickwang123@gmail.com>
2024-10-15 13:35:27 +0900

    feature

---
+++ src/main/java/com/market/apple/global/initData/Dev.java
@@ -0,0 +1,34 @@
+package com.market.apple.global.initData;
+
+import com.market.apple.domain.article.service.ArticleService;
+import com.market.apple.domain.member.entity.Member;
+import com.market.apple.domain.member.service.MemberService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class Dev {
+
+    @Bean
+    public CommandLineRunner init(MemberService memberService, ArticleService articleService) {
+        return args -> {
+            Member admin = memberService.create("admin", "1234", "admin@admin.com");
+            Member member1 =  memberService.create("member1", "1234",  "member1@member1.com");
+            Member member2 = memberService.create("member2", "1234", "member2@member2.com");
+
+            // 게시글 등록
+            articleService.create("제목1",  "내용1", true, admin);
+            articleService.create("제목2",  "내용2", false, member1);
+            articleService.create("제목3",  "내용3", false, member1);
+            articleService.create("제목4",  "내용4", false, member2);
+            articleService.create("제목5",  "내용5", false, member2);
+            
+            // 퀴즈 등록
+
+        };
+    }
+
+}
---
+++ src/main/java/com/market/apple/global/jpa/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.market.apple.global.jpa;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
+
